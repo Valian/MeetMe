f77d0aad46f7866dea5e57b635c1054ac2279ad8
leniecdrive <leniecdrive@gmail.com>
2014-01-15 18:04:23 +0100

    to jest mega komit

---
+++ SimpleWebApp/src/main/java/sample/web/app/components/SimpleComponent.java
@@ -0,0 +1,16 @@
+package sample.web.app.components;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleComponent
+{
+	
+	public String getString()
+	{
+		
+		return "Stringus";
+		
+	}
+	
+}
